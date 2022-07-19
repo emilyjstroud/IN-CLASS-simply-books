48c46e36f9dc5d50044c71e4d2701abcb0ad3b63
Emily Stroud <emilystroud93@gmail.com>
2022-07-18 21:07:08 -0500

    getting authors by UID

--- pages/authors.js
+++ pages/authors.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthorCard from '../components/AuthorCard';
+import { getAuthors } from '../api/authorData';
+import { useAuth } from '../utils/context/authContext';
 
-export default function Authors() {
+function AuthorsPage() {
+  const [authors, setAuthors] = useState([]);
+
+  const { user } = useAuth;
+
+  // const getAllAuthors = () => {
+  //   getAuthors(user.uid).then(setAuthors);
+  // };
+
+  useEffect(() => {
+    getAuthors(user.uid).then(setAuthors);
+  }, [user.uid]);
+
+  // export default function Authors() {
   return (
     <div>
       <AuthorCard />
+      {
+        authors.map((author) => <AuthorCard key={author.firebaseKey} authorObj={author} />)
+      }
       authors
     </div>
   );
 }
+
+export default AuthorsPage;
